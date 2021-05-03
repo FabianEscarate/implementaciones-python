adb264042380ee05ce6c451c9324312927558a43
Fabian Escarate <fescarate@outlook.com>
2021-05-03 15:06:41 -0400

    feat: little manager path

---
+++ temporalFiles/csv_files/manage_file.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+from os import path
+
+
+class FILES:
+    file_1 = 'file_1'
+    file_2 = 'file_2'
+    file_3 = 'file_3'
+    canal_10_file_1 = 'canal_10/file_1'
+    canal_20_file_1 = 'canal_20/minable/file_1'
+
+
+def make_dir_exist(file_path: str):
+    destination_path = Path(file_path).parent.absolute()
+    if destination_path.exists() is False:
+        destination_path.mkdir(777, True, True)
+
+    return destination_path.exists()
+
+
+def getPath(_path):
+    if _path is not None and _path is not '':
+        if _path in list(vars(FILES).values()):
+            currentPath = Path(__file__).parent.absolute()
+            resultPath = path.join(currentPath, _path+'.csv')
+            if make_dir_exist(resultPath) is True:
+                return resultPath
+            raise Exception('No se pudo crear Directorio')
+        raise Exception('query no registrada en mantenedor QUERY')
+    raise Exception('parametro Query invalido')
