48d139312b99069125ed2877b70870ed2e1ec693
Fabian Escarate <fescarate@outlook.com>
2021-05-03 16:13:45 -0400

    feat: termino de little mini framework

--- temporalFiles/python_script/funciones_modelo/minable_nacional.py
+++ temporalFiles/python_script/funciones_modelo/minable_nacional.py
@@ -1,4 +1,4 @@
-# import pandas as pd
+import pandas as pd
 # import psycopg2
 # import config
 # import holidays
@@ -7,9 +7,24 @@
 # from numpy  import inf
 # from dateutil.relativedelta import relativedelta
 # from operator import attrgetter
+# query manager
+from setting import local_settings
 from sql_scripts.manage_query import QUERYS, getQuery
+# file path manager
+from csv_files.manage_file import FILES, getPath
+# settings
+try:
+    import setting
+    LOCAL_SETTINGS = local_settings
+except Exception as err:
+    print(err)
+
 
 def minable_nacional():
+
+    # imprimir settings
+    print('settings', LOCAL_SETTINGS)
+
     # Connect to the PostgreSQL database
     # try:
     #     conn = psycopg2.connect(host='127.0.0.1', database='test', user='test_user', port=5432, password='test_password')
@@ -26,13 +41,19 @@ def minable_nacional():
 
     def descargar_transaccional():
         print('conectado a servidor')
-        
+
         script = getQuery(QUERYS.transaccional_canal_10)
-      
+        print('script', script)
         return script
 
-    #Leer csv
+    # Leer csv
     df_mensual = descargar_transaccional()
-    print('script', df_mensual)
-    # df_minable.to_csv(r'../df/resultado.csv',index=False)
-    # return df_minable
+    mydataset = {
+        'cars': ["BMW", "Volvo", "Ford"],
+        'passings': [7, 7, 7]
+    }
+
+    df_minable_example = pd.DataFrame(mydataset)
+
+    df_minable_example.to_csv(getPath(FILES.canal_20_file_1), index=False)
+    return df_minable_example
